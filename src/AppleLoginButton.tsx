6db8c6fc6d83e4c39c072859f393509cc8030110
ADR!AN <111903096+adrianvrj@users.noreply.github.com>
2025-07-10 11:25:36 -0600

    #4 CavosWallet object

--- src/AppleLoginButton.tsx
+++ src/AppleLoginButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TouchableOpacity, Text, View, ActivityIndicator, StyleSheet, ViewStyle, TextStyle } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import Svg, { Path } from 'react-native-svg';
+import { CavosWallet } from './CavosWallet';
 
 export type AppleLoginButtonProps = {
     orgToken: string;
@@ -58,6 +59,13 @@ export const SignInWithApple: React.FC<AppleLoginButtonProps> = ({
                 const userDataStr = params.get('user_data');
                 if (userDataStr) {
                     const userData = JSON.parse(decodeURIComponent(userDataStr));
+                    const cavosWallet = new CavosWallet(
+                        userData.wallet.address,
+                        userData.wallet.network,
+                        userData.email,
+                        userData.wallet.hashedPk,
+                        orgToken
+                    )
                     if (onSuccess) {
                         onSuccess(userData);
                     }
---
+++ src/CavosWallet.ts
@@ -0,0 +1,46 @@
+export class CavosWallet {
+    // Públicos
+    public address: string;
+    public network: string;
+    public email: string;
+
+    // Privados
+    private hashedPk: string;
+    private orgToken: string;
+
+    constructor(
+        address: string,
+        network: string,
+        email: string,
+        hashedPk: string,
+        orgToken: string,
+    ) {
+        this.address = address;
+        this.network = network;
+        this.email = email;
+        this.hashedPk = hashedPk;
+        this.orgToken = orgToken;
+    }
+
+    // Método público para ejecutar calls de contratos
+    public async executeCalls(calls: any[]): Promise<any> {
+        const res = await fetch(
+            `https://services.cavos.xyz/api/v1/external/execute`,
+            {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${this.orgToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    network: this.network,
+                    calls,
+                    address: this.address,
+                    hashedPk: this.hashedPk,
+                }),
+            }
+        );
+        if (!res.ok) throw new Error('Error executing calls');
+        return await res.json();
+    }
+} 
